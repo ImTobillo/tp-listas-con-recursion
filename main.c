af0c2353d5df96cdf3a0d2e53c2e74d9e240e7ec
tobias <tobinoyacabj@gmail.com>
2021-09-21 03:52:50 -0300

    tp completo

--- main.c
+++ main.c
@@ -55,7 +55,7 @@ void mostrarListaInvert (nodo* lista)
 {
     if (lista)
     {
-        mostrarListaInvert(lista->sig);
+        mostrarListaInvert(lista->sig); // llamada recursiva y acercamiento
         printf("%i\t", lista->dato);
     }
 }
@@ -63,9 +63,113 @@ void mostrarListaInvert (nodo* lista)
 int retSumaLista (nodo* lista)
 {
     if (lista)
-        return lista->dato + retSumaLista(lista->sig);
+        return lista->dato + retSumaLista(lista->sig); // llamada recursiva y acercamiento
+    else  // condicion de corte (lista == null)
+        return 0; // solucion trivial
+}
+
+void mostrarListaSoloPares (nodo* lista, int contador) // contador inicializado en 1
+{
+    if (lista)
+    {
+        if (contador % 2 == 0)
+            printf("%i\t", lista->dato);
+
+        mostrarListaSoloPares(lista->sig, ++contador);
+    }
+}
+
+void agregarNodoAlFinal (nodo** lista, nodo* agreg)
+{
+    if (*lista)
+    {
+        nodo* aux = *lista;
+
+        while (aux->sig)
+            aux = aux->sig;
+
+        aux->sig = agreg;
+    }
     else
-        return 0;
+        *lista = agreg;
+}
+
+void agregarNodoAlPrincipio (nodo** lista, nodo* agreg)
+{
+    if (*lista)
+    {
+        agreg->sig = *lista;
+
+        *lista = agreg;
+    }
+    else
+        *lista = agreg;
+}
+
+void invertirListaRec (nodo** lista, nodo* listaAux) // aux inicializado en NULL
+{
+    nodo* aux;
+
+    if (*lista)
+    {
+        aux = *lista; // guardo nodo de lista
+
+        *lista = (*lista)->sig; // avanzo en lista  ----  acercamiento (¿??)
+
+        aux->sig = listaAux; // agrego nodo a lista invertida
+
+        listaAux = aux; // actualizo lista invertida
+
+        invertirListaRec(lista, listaAux); // llamada recursiva
+    }
+    else // corte
+        *lista = listaAux; // solucion trivial
+}
+
+void borrarNodoRec (nodo** lista, nodo** ant, nodo** act, int dato)
+{
+    nodo* aux;
+
+    if ((*lista)->dato == dato) // 1er condicion de corte (¿?)
+    {
+        aux = *lista;
+        *lista = *act;     // 1ra solucion trivial (¿?)
+        free(ant);
+    }
+    else if (*act)
+    {
+        if ((*act)->dato == dato) // 2da condicion de corte
+        {
+            aux = *act;
+            (*ant)->sig = (*act)->sig;    // 2da solucion trivial
+            free(aux);
+        }
+        else
+            borrarNodoRec(lista, &(*ant)->sig, &(*act)->sig, dato); // llamada recursiva y acercamientos
+    }
+}
+
+void insertarOrdenadoRec (nodo** lista, nodo** ant, nodo** act, int dato)
+{
+    if (dato < (*lista)->dato)
+    {
+        nodo* aux = crearNodo(dato);
+        aux->sig = *lista;
+        *lista = aux;
+    }
+    else if (*act)
+    {
+        if (dato < (*act)->dato)
+        {
+            nodo* aux = crearNodo(dato);
+            aux->sig = *act;
+            (*ant)->sig = aux;
+        }
+        else
+            insertarOrdenadoRec(lista, &(*ant)->sig, &(*act)->sig, dato);
+    }
+    else
+        (*ant)->sig = crearNodo(dato);
 }
 
 /// MAIN
@@ -75,11 +179,11 @@ int main()
     nodo* lista;
     inicLista(&lista);
 
-    agregarNodo(&lista, 4);
+    agregarNodo(&lista, 9);
+    agregarNodo(&lista, 8);
     agregarNodo(&lista, 6);
-    agregarNodo(&lista, 3);
+    agregarNodo(&lista, 5);
     agregarNodo(&lista, 2);
-    agregarNodo(&lista, 7);
     agregarNodo(&lista, 1);
 
     mostrarLista(lista);
@@ -89,6 +193,21 @@ int main()
 
     printf("\nLa suma de los datos de la lista es: %i\n", retSumaLista(lista));
 
+    printf("\n");
+    mostrarListaSoloPares(lista, 1);
 
+    printf("\nLISTA SIN INVERTIR\n");
+    mostrarLista(lista);
+    printf("\nLISTA INVERTIDA\n");
+    invertirListaRec(&lista, NULL);
+    mostrarLista(lista);
+
+    printf("\nDESPUES DE BORRAR EL 7\n");
+    borrarNodoRec(&lista, &lista, &lista->sig, 7);
+    mostrarLista(lista);
+
+    printf("\nDESPUES DE INSERTAR\n");
+    insertarOrdenadoRec(&lista, &lista, &lista->sig, 3);
+    mostrarLista(lista);
     return 0;
 }
